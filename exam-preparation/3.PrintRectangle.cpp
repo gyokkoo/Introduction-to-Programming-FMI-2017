c0276d3976013f0c144bdf822be37b56379d2613
Gyokan <gyokan.nezhdetov@gmail.com>
2018-01-28 12:52:33 +0200

    Added third task

---
+++ exam-preparation/3.PrintRectangle.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using std::cout;
+
+const int CONSOLE_WIDTH = 80;
+const int CONSOLE_HEIGH = 25;
+
+void printRectangle(int n, int m, char symbol)
+{
+    int x = (CONSOLE_HEIGH - n) / 2;
+    int y = (CONSOLE_WIDTH - m) / 2;
+
+    for (int i = 0; i < x; i++)
+    {
+        cout << '\n';
+    }
+
+    for (int i = x; i < x + n; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            cout << " ";
+        }
+        for (int j = y; j < y + m; j++)
+        {
+            cout << symbol;
+        }
+        for (int j = y + m; j < CONSOLE_WIDTH; j++)
+        {
+            cout << " ";
+        }
+    }
+}
+
+int main()
+{
+    int n = 10;
+    int m = 50;
+    char symbol = 'O';
+
+    printRectangle(n, m, symbol);
+
+    return 0;
+}
